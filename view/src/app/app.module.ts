0eeb42b417bf109bd16ac4181932f25530e1e0f7
Chelsea Marfil <chelseamarfil@yahoo.com>
2019-03-13 15:09:00 -0700

    Configured routing

--- view/src/app/app.module.ts
+++ view/src/app/app.module.ts
@@ -1,3 +1,4 @@
+// Default Angular imports
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,6 +6,23 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './components/login/login.component';
 import { MainComponent } from './components/main/main.component';
+
+// Manually added this Angular import.
+import { RouterModule, Routes } from '@angular/router';
+
+  /** 
+   * Manually added
+   * Added the routing configuration as an array of Routes.
+   * Uses the factory method RouterModule.forRoot to hand over our routing config.
+   * Connects a URL extension (like 'login') to a component (LoginComponent).
+   * A redirect is configured from the default app route to the route displaying content from LoginComponent.
+   */
+
+  const appRoutes: Routes = [
+    { path: 'login', component: LoginComponent}, 
+    { path: 'main', component: MainComponent },
+    { path: '', redirectTo: '/login', pathMatch: 'full' }
+  ];
 
 @NgModule({
   declarations: [
@@ -14,9 +32,19 @@ import { MainComponent } from './components/main/main.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
+    /** 
+     * Manually added
+     * To activate the routing configuration for the Angular app.
+     */
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+
 })
 export class AppModule { }
