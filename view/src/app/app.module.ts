411e3676e17475f0a8850c831cc43d9746d5c720
Chelsea Marfil <chelseamarfil@yahoo.com>
2019-03-20 15:10:31 -0700

    Added Angular toolbar and created separate file (material.module.ts) for all Material components

--- view/src/app/app.module.ts
+++ view/src/app/app.module.ts
@@ -1,31 +1,33 @@
-// Default Angular imports
+// Start of default Angular imports
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './components/login/login.component';
 import { MainComponent } from './components/main/main.component';
+// End of Default Angular imports
 
-// Manually added this Angular import.
+// Manually added this Angular import - for routing.
 import { RouterModule, Routes } from '@angular/router';
 
-// Manually added this Angular import which is used in app.component.html
-import { MatToolbarModule } from '@angular/material';
+// // Manually added this Angular import which is used in app.component.html
+// import { MatToolbarModule } from '@angular/material';
+// MOVED THIS CODE TO MATERIAL.MODULE.TS
 
+// Manually added for login page.
+import { CustomMaterialModule } from './material.module';
+import { FormsModule } from '@angular/forms';
 
   /** 
    * Manually added
    * Added the routing configuration as an array of Routes.
    * Uses the factory method RouterModule.forRoot to hand over our routing config.
    * Connects a URL extension (like 'login') to a component (LoginComponent).
-   * A redirect is configured from the default app route to the route displaying content from LoginComponent.
    */
-
   const appRoutes: Routes = [
     { path: 'login', component: LoginComponent}, 
     { path: 'main', component: MainComponent },
-    { path: '', redirectTo: '/login', pathMatch: 'full' }
+    { path: '', redirectTo: '/login', pathMatch: 'full' } // A redirect is configured from the default app route to the route displaying content from LoginComponent.
   ];
 
 @NgModule({
@@ -47,8 +49,12 @@ import { MatToolbarModule } from '@angular/material';
       { enableTracing: true } // <-- debugging purposes only
     ),
     
-    // Manually added
-    MatToolbarModule
+    // Manually added - used in app.component.html
+    //MatToolbarModule,
+
+    // For login page
+    CustomMaterialModule,
+    FormsModule
 
   ],
   providers: [],
