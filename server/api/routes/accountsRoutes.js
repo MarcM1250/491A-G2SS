ee13ef21acab962a931fb48041f7cfec673de611
DuongPham14 <31968090+DuongPham14@users.noreply.github.com>
2019-04-10 14:40:17 -0700

    Backend API
    Adding Account/Upload/Download models, controllers, and routes.

---
+++ server/api/routes/accountsRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const Authentication = require('../middleware/authentication');
+const AccountsController = require('../controllers/accountsController');
+
+router.get('/', AccountsController.get_all);
+router.get('/:username', Authentication.check_user, AccountsController.get_account);
+router.post('/create', AccountsController.create_account);
+router.post('/login', AccountsController.login);
+router.delete('/:username', AccountsController.delete_account);
+
+module.exports = router;
