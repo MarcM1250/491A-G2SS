324a8d31bc5f9dbd1fcfb7515204227f9364b732
ashtrax3000 <ashtrax3000@gmail.com>
2019-09-08 22:09:50 -0700

    SSL enabled
    SSL working using OpenSSL
    
    TODO -> Reverse Proxy With Express port 443 -> 3000

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
-const http = require('http');
-const app = require('./app');
 
+const https = require('https');
+const fs = require("fs");
+const app = require('./app');
 const port = process.env.PORT || 3000;
 
-const server = http.createServer(app);
+const server = https.createServer({
+    key: fs.readFileSync('./hidden/key.pem'),
+    cert: fs.readFileSync('./hidden/cert.pem'),
+    passphrase: 'secretPhrase'
+}, app);
 
 server.listen(port, function() {
     console.log('Server started on port ' + port);
 });
-
