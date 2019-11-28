d41eaae42f02a49a33b3cc3a80be4438c494de11
ashtrax3000 <ashtrax3000@gmail.com>
2019-11-27 16:04:15 -0800

    more unit test done

--- web/src/app/services/authentication.service.ts
+++ web/src/app/services/authentication.service.ts
@@ -8,6 +8,7 @@ import { User } from '../models/user.model';
 import decode from 'jwt-decode';
 
 export const API_URL = 'http://localhost:3000/api';
+//export const API_URL ='http://protean-sensor-259821.appspot.com/api';
 
 @Injectable()
 
